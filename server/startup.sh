5ba62f77567ea71d97a3a18e94d9c80c0b273bd8
Ditto Asnar <ditto.asnar@gmail.com>
2025-08-19 08:31:42 +0700

    fix(build): simplify build process and fix startup inconsistency

--- server/startup.sh
+++ server/startup.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-set -e
-
-echo "--- Current Directory ---"
-pwd
-echo
-
-echo "--- Root Directory Content ---"
-ls -lR /home/site/wwwroot
-echo
-
-echo "--- package.json content ---"
-cat /home/site/wwwroot/package.json
-echo
-
-echo "--- Starting App ---"
 npm start
